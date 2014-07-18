b5d9b5bfdcebf353f6d556eb432407ea578071f9
Arshad Chummun <arshad@chummun.com>
2014-07-18 10:00:15 +0400

    Added support for flexible grid system

--- templates/radix-views-grid-plugin-style.tpl.php
+++ templates/radix-views-grid-plugin-style.tpl.php
@@ -9,7 +9,7 @@
   <div class="<?php print $views_group_col_class; ?> views-group">
     <h2><?php print $title; ?></h2>
     <?php foreach ($rows as $row): ?>
-      <div class="row views-row">
+      <div class="<?php print $views_row_class; ?> views-row">
         <?php foreach ($row as $item): ?>
           <div class="<?php print $views_row_col_class; ?> views-col">
             <?php print $item; ?>
@@ -21,11 +21,17 @@
 <?php else: ?>
   <?php foreach ($rows as $row): ?>
     <div class="row views-row">
+      <?php if (is_array($row)): ?>
       <?php foreach ($row as $item): ?>
         <div class="<?php print $views_row_col_class; ?> views-col">
           <?php print $item; ?>
         </div>
       <?php endforeach; ?>
+      <?php else: ?>
+        <div class="views-col">
+          <?php print $row; ?>
+        </div>
+      <?php endif; ?>
     </div>
   <?php endforeach; ?>
 <?php endif; ?>
